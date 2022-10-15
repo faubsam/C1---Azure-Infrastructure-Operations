a6feffc99ef5fd8bb1af1eecf3844d415308d184
faubsam <faubertsamuel@gmail.com>
2022-10-14 21:20:02 -0400

    fixed main network interfaces

--- project/main.tf
+++ project/main.tf
@@ -21,17 +21,17 @@ resource "azurerm_resource_group" "az-devops-c1-rg" {
   }
 }
 
-resource "azurerm_virtual_network" "az-devops-c1-vn" {
+resource "azurerm_virtual_network" "vn" {
   name                = "${var.prefix}-vn"
   resource_group_name = azurerm_resource_group.az-devops-c1-rg.name
   location            = azurerm_resource_group.az-devops-c1-rg.location
-  address_space       = ["10.0.0.0/24"]
+  address_space       = var.address_space
   tags = {
     Project = "Azure Devops C1"
   }
 }
 
-resource "azurerm_network_security_group" "az-devops-c1-sg" {
+resource "azurerm_network_security_group" "sg" {
   name                = "${var.prefix}-sg"
   resource_group_name = azurerm_resource_group.az-devops-c1-rg.name
   location            = azurerm_resource_group.az-devops-c1-rg.location
@@ -44,7 +44,7 @@ resource "azurerm_network_security_group" "az-devops-c1-sg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "*"
-    source_address_prefix      = var.address_prefixes
+    source_address_prefix      = "10.0.1.0/24"
     destination_address_prefix = "*"
   }
 
@@ -65,7 +65,7 @@ resource "azurerm_network_security_group" "az-devops-c1-sg" {
   }
 }
 
-resource "azurerm_public_ip" "az-devops-c1-public-ip" {
+resource "azurerm_public_ip" "public-ip" {
   name                = "${var.prefix}-public-ip"
   resource_group_name = azurerm_resource_group.az-devops-c1-rg.name
   location            = azurerm_resource_group.az-devops-c1-rg.location
@@ -79,13 +79,14 @@ resource "azurerm_public_ip" "az-devops-c1-public-ip" {
 resource "azurerm_subnet" "internal" {
   name                 = "${var.prefix}-internal"
   resource_group_name = azurerm_resource_group.az-devops-c1-rg.name
-  address_prefixes     = var.address_prefixes
-  virtual_network_name = azurerm_virtual_network.az-devops-c1-vn.name
+  address_prefixes     = ["10.0.1.0/24"]
+  virtual_network_name = azurerm_virtual_network.vn.name
 
 }
 
-resource "azurerm_network_interface" "az-devops-c1-inet" {
-  name                = "${var.prefix}-inet"
+resource "azurerm_network_interface" "inet" {
+  count = var.vm_count
+  name                = "${var.prefix}-inet-${count.index}"
   resource_group_name = azurerm_resource_group.az-devops-c1-rg.name
   location            = azurerm_resource_group.az-devops-c1-rg.location
 
@@ -100,14 +101,14 @@ resource "azurerm_network_interface" "az-devops-c1-inet" {
   }
 }
 
-resource "azurerm_lb" "az-devops-c1-lb" {
+resource "azurerm_lb" "lb" {
   name                = "${var.prefix}-lb"
   resource_group_name = azurerm_resource_group.az-devops-c1-rg.name
   location            = azurerm_resource_group.az-devops-c1-rg.location
 
   frontend_ip_configuration {
-    name                 = azurerm_public_ip.az-devops-c1-public-ip.name
-    public_ip_address_id = azurerm_public_ip.az-devops-c1-public-ip.id
+    name                 = azurerm_public_ip.public-ip.name
+    public_ip_address_id = azurerm_public_ip.public-ip.id
   }
 
   tags = {
@@ -115,7 +116,12 @@ resource "azurerm_lb" "az-devops-c1-lb" {
   }
 }
 
-resource "azurerm_availability_set" "az-devops-c1-as" {
+resource "azurerm_lb_backend_address_pool" "main" {
+  loadbalancer_id     = azurerm_lb.lb.id
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_availability_set" "as" {
   name                = "${var.prefix}-as"
   resource_group_name = azurerm_resource_group.az-devops-c1-rg.name
   location            = azurerm_resource_group.az-devops-c1-rg.location
@@ -126,29 +132,30 @@ resource "azurerm_availability_set" "az-devops-c1-as" {
 }
 
 data "azurerm_image" "image" {
-  name                = var.packer_image_name
+  name = "az-devops-c1-ubuntu-image-18"
   resource_group_name = var.images_resource_group
 
 }
 
-resource "azurerm_virtual_machine" "az-devops-c1-vm" {
-  count               = var.counter
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = var.vm_count
   name                = "${var.prefix}-vm-${count.index}"
   resource_group_name = azurerm_resource_group.az-devops-c1-rg.name
   location            = azurerm_resource_group.az-devops-c1-rg.location
-  vm_size                = "Standard_DS1_v2"
+  size                = "Standard_DS1_v2"
+  admin_username      = "azdevopsc1admin"
+  admin_password      = var.admin_password
+  source_image_id     = var.packer_image_name
   network_interface_ids = [
-    azurerm_network_interface.az-devops-c1-inet.id,
-  ]
+    element(azurerm_network_interface.inet.*.id, count.index)
+  ]  
+  availability_set_id = azurerm_availability_set.as.id
+  disable_password_authentication = false
 
-  
-  source_image_id = data.azurerm_image.image.id
-  
 
-  storage_os_disk {
-    name = "os_disk-${count.index}"
-    create_option = "Attach"
-    
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
   }
   
   tags = {
@@ -156,8 +163,9 @@ resource "azurerm_virtual_machine" "az-devops-c1-vm" {
   }
 }
 
-resource "azurerm_managed_disk" "az-devops-c1-md" {
-  name                 = "${var.prefix}-md"
+resource "azurerm_managed_disk" "md" {
+  count = var.vm_count
+  name                 = "${var.prefix}-managed-disk-${count.index}"
   resource_group_name  = azurerm_resource_group.az-devops-c1-rg.name
   location             = azurerm_resource_group.az-devops-c1-rg.location
   storage_account_type = "Standard_LRS"
@@ -169,10 +177,10 @@ resource "azurerm_managed_disk" "az-devops-c1-md" {
   }
 }
 
-resource "azurerm_virtual_machine_data_disk_attachment" "az-devops-c1-mda" {
-  count=var.counter
-  managed_disk_id    = azurerm_managed_disk.az-devops-c1-md.id
-  virtual_machine_id = azurerm_virtual_machine.az-devops-c1-vm-[count.index]
-  lun                = "10"
+resource "azurerm_virtual_machine_data_disk_attachment" "mda" {
+  count=var.vm_count
+  managed_disk_id    = element(azurerm_managed_disk.md.*.id, count.index)
+  virtual_machine_id = element(azurerm_linux_virtual_machine.vm.*.id, count.index)
+  lun                = "10" 
   caching            = "ReadWrite"
 }
--- project/variables.tf
+++ project/variables.tf
@@ -8,22 +8,27 @@ variable "location" {
     default = "East US"
 }
 
-variable "counter" {
+variable "vm_count" {
     description = "The number of virtual machines to create within the scale set"
     default = 2
 }
 
-variable "address_prefixes" {
-    description = "The address block for the internal subnet"
-    default = ["10.0.1.0/24"]
+variable "address_space" {
+    description = "The address block for the virtual network"
+    default = ["10.0.0.0/16"]
 }
 
 variable "packer_image_name" {
-    description = "The name of the packer image to use for the virtual machines"
-    default = "az-devops-c1-ubuntu-image-18"
+    description = "The id of the packer image to use for the virtual machines"
+    default = "/subscriptions/1b33bac2-45ca-42d7-9099-3239addac4ee/resourceGroups/az-devops-c1-images-rg/providers/Microsoft.Compute/images/az-devops-c1-ubuntu-image-18"
 }
 
 variable "images_resource_group" {
     description = "The resource group where packer images are stored"
     default = "az-devops-c1-images-rg"
+}
+
+variable "admin_password" {
+    description = "The initial administrator password for the vm"
+    default = "@dm1n123$"
 }
